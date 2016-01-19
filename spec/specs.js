b72bb4ee02dac5293e74a169c8604967a1649fa0
Joshua Gustafson and Joseph Karasek <silversevenfox@yahoo.com>
2016-01-19 13:50:38 -0800

    add new spec for removing special characters

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe("normalizeText", function(){
   it("return string with uppercase changed to lowercase", function(){
     expect(normalizeText("ABCDEFG")).to.equal("abcdefg");
   });
+  it("return string with all special characters removed", function(){
+    expect(normalizeText("aabb#$%.aabb")).to.equal("aabbaabb");
+  });
 });

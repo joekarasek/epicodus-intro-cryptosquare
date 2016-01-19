87f46000e9b31ebd820d5816c61acddac439d374
Joshua Gustafson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-19 13:56:13 -0800

    add spec to remove all spaces, spec already met

--- spec/specs.js
+++ spec/specs.js
@@ -5,4 +5,7 @@ describe("normalizeText", function(){
   it("return string with all special characters removed", function(){
     expect(normalizeText("aabb#$%.aabb")).to.equal("aabbaabb");
   });
+  it("return a string with all spaces removed", function(){
+    expect(normalizeText("aa bb aa bb")).to.equal("aabbaabb");
+  });
 });

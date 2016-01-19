ccfdc8835668755834dde468134937c968e784a7
Joshua Gustafson and Joseph Karasek <joseph.karasek@gmail.com>
2016-01-19 14:18:42 -0800

    add function return true to isSquare if input is a square

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ var normalizeText = function(textToNormalize) {
   // remove special characters and turn to lowercase
   return textToNormalize.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 }
-var isSquare = function() {
-  return false;
+var isSquare = function(number) {
+  if(Math.sqrt(number) %1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
